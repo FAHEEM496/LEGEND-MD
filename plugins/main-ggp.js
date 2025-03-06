ccbe32acdd058ade4e15d46395e8f28a7bfbc2f3
FAHEEM496 <mf7227164@gmail.com>
2025-03-05 21:32:23 -0800

    Create main-ggp.js

---
+++ plugins/main-ggp.js
@@ -0,0 +1,15 @@
+let handler = async m => m.reply(`𝚂𝚄𝙿𝙿𝙾®𝚃 𝙶𝚁𝙾𝚄𝙿 𝙻𝙸𝙽𝙺
+ ★☆⚡𝑷-𝑴𝑫⚡☆★
+
+*${mssg.link}*: https://chat.whatsapp.com/IopepT0DcmWDrYz2p9jcyP
+
+*${mssg.link}*: https://whatsapp.com/channel/0029Vb3CNbCFSAt1rR75SQ0P
+
+*OWNER*
+${developer}
+`.trim())
+handler.help = ['support']
+handler.tags = ['main']
+handler.command = ['support', 'sup'] 
+
+export default handler

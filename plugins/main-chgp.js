e92d8741b21f2dfda61723244e7edc3cc1dd412d
FAHEEM496 <mf7227164@gmail.com>
2025-03-05 21:30:42 -0800

    Create main-chgp.js

---
+++ plugins/main-chgp.js
@@ -0,0 +1,12 @@
+let handler = async m => m.reply(`𝙁𝙊𝙇𝙇𝙊𝙒 𝙏𝙃𝙀 𝘾𝙃𝘼𝙉𝙉𝙀𝙇 𝙏𝙊 𝙂𝙀𝙏 𝙇𝘼𝙏𝙀𝙎𝙏 LEGEND-𝙈𝘿 𝙐𝙋𝘿𝘼𝙏𝙀𝙎 
+
+*${mssg.link}*: https://whatsapp.com/channel/0029Vb3CNbCFSAt1rR75SQ0P
+
+*𝙊𝙒𝙉𝙀𝙍*
+${developer}
+`.trim())
+handler.help = ['channel']
+handler.tags = ['main']
+handler.command = ['channel', 'ch'] 
+
+export default handler
